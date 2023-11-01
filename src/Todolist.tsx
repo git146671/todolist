f027685fe0312929f644534bff02d67d629258f5
Anton Plekhanov <146671@mail.ru>
2023-11-02 00:54:48 +0300

    переведено на styled components

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,12 +1,13 @@
 import React, {ChangeEvent, KeyboardEvent, useState} from "react";
 import {FilterValuesType} from "./App";
+import styled from "styled-components";
 
+//************* COMPONENTS TYPES ***************//
 export type TaskType = {
     id: string;
     title: string;
     isDone: boolean;
 }
-
 type TodolistPropsType = {
     title: string;
     tasks: Array<TaskType>;
@@ -16,11 +17,15 @@ type TodolistPropsType = {
     changeStatus: (id: string) => void;
     filter: FilterValuesType;
 }
+
+//************* COMPONENT ***************//
 export let Todolist = (props: TodolistPropsType) => {
 
+    //*** LOCAL USE STATES ***
     const [newTaskTitle, setNewTaskTitle] = useState("");
     const [error, setError] = useState<string | null>(null);
 
+    //*** FUNCTIONS ***
     const onNewTitleChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
         setNewTaskTitle(e.currentTarget.value);
     }
@@ -50,16 +55,17 @@ export let Todolist = (props: TodolistPropsType) => {
     const onClickChangeFilterActive = () => props.changeFilter("active");
     const onClickChangeFilterCompleted = () => props.changeFilter("completed");
 
-
+    //*** JSX ***
     return <div>
         <h3>{props.title}</h3>
         <div>
-            <input className={error ? "error" : ""}
+            <StyledInput border={error ? "red 1px solid" : ""}
                    value={newTaskTitle}
                    onChange={onNewTitleChangeHandler}
                    onKeyUp={onKeyUpInputHandler}/>
+
             <button onClick={onAddTaskClickBtn}>+</button>
-            {error && <div className="error-message">{error}</div>}
+            {error && <StyledRequiredFieldMsgDiv className="error-message">{error}</StyledRequiredFieldMsgDiv>}
         </div>
         <ul>
             {
@@ -68,18 +74,46 @@ export let Todolist = (props: TodolistPropsType) => {
                         props.removeTask(t.id)
                     }
                     const onClickCheckBox = () => props.changeStatus(t.id);
-                    return <li className={t.isDone ? "is-done" : ""}>
+                    return <StyledTaskLi opacity={t.isDone ? "0.5" : ""}>
                         <input type="checkbox" checked={t.isDone} onChange={onClickCheckBox}/>
                         <span>{t.title}</span>
                         <button onClick={onRemoveTaskClickBtn}>x</button>
-                    </li>
+                    </StyledTaskLi>
                 })
             }
         </ul>
         <div>
-            <button className={props.filter === "all" ? "active-filter" : ""} onClick={onClickChangeFilterAll}>All</button>
-            <button className={props.filter === "active" ? "active-filter" : ""} onClick={onClickChangeFilterActive}>Active</button>
-            <button className={props.filter === "completed" ? "active-filter" : ""} onClick={onClickChangeFilterCompleted}>Completed</button>
+            <StyledFilterBtn bgc={props.filter === "all" ? "aquamarine" : ""} onClick={onClickChangeFilterAll}>All</StyledFilterBtn>
+            <StyledFilterBtn bgc={props.filter === "active" ? "aquamarine" : ""} onClick={onClickChangeFilterActive}>Active</StyledFilterBtn>
+            <StyledFilterBtn bgc={props.filter === "completed" ? "aquamarine" : ""} onClick={onClickChangeFilterCompleted}>Completed</StyledFilterBtn>
         </div>
     </div>
 }
+
+//************* STYLED COMPONENTS TYPES ***************//
+type StyledInputType = {
+    border: string;
+}
+type StyledTaskLiType = {
+    opacity: string;
+}
+type StyledFilterBtnType = {
+    bgc: string;
+}
+
+//**************** STYLED COMPONENTS ******************//
+const StyledInput = styled.input<StyledInputType>`
+      border: ${props => props.border};
+`
+
+const StyledRequiredFieldMsgDiv = styled.div`
+  color: red;
+`
+
+const StyledTaskLi = styled.li<StyledTaskLiType>`
+  opacity: ${props => props.opacity};
+`
+
+const StyledFilterBtn = styled.button<StyledFilterBtnType>`
+  background-color: ${props => props.bgc};
+`
